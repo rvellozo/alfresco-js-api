33bd45cce0f8073e0bacb3e9c7bcb4e419ef2b91
Ricardo Vellozo <ricardo.vellozo@alfresco.com>
2022-01-21 10:42:05 +0000

    c1

--- login.js
+++ login.js
@@ -1,7 +1,10 @@
-this.alfrescoJsApi = new AlfrescoApi({ provider:'ECM', hostEcm: 'http://localhost:8080' });
+const alfrescoJsApi = new AlfrescoApi({ provider:'ECM', hostEcm: 'http://localhost:8080' });
 
-this.alfrescoJsApi.login('admin', 'admin').then(function (data) {
-    alert('API called successfully to login into Alfresco Content Services.');
-}, function (error) {
-    console.error(error);
-});
+alfrescoJsApi.login('admin', 'admin').then(
+    data => {
+        console.log('API called successfully Login in  BPM and ECM performed ');
+    },
+    error => {
+        console.error(error);
+    }
+);
